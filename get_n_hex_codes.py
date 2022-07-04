403f2abf7da08c8f8a8f4ed11783044abb681e55
Luis Antonio Haddock III <55507927+haddocksoto@users.noreply.github.com>
2022-07-04 15:08:06 -0500

    Create get_n_hex_codes.py

---
+++ get_n_hex_codes.py
@@ -0,0 +1,9 @@
+import random
+
+#generating n number of distinct hex codes for our barcodes
+
+number_of_colors = 3000 #change value as desired
+color = ["#" + ''.join([random.choice('0123456789ABCDEF') for x in range(6)])
+             for c in range(number_of_barcodes)]
+
+#output: list of hex codes 
